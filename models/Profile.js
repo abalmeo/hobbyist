5d1afeee996432ed385aac1f874f37a181b99d94
Adam Garling <adamgarling@adams-mbp.attlocal.net>
2018-07-16 18:08:33 -0700

    setting up profile routes

---
+++ models/Profile.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema; 
+
+//Profile Schema 
+const ProfileSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    },
+    handle: {
+        type: String,
+        required: true,
+        max: 30
+    },
+    location: {
+        type: String,
+        required: false,
+        max: 30
+    },
+    bio: {
+        type: String,
+        required: false
+    },
+    skills: {
+        type: [String],
+        required: true
+    },
+    equipment: {
+        type: [String],
+        required: false
+    },
+    interest: {
+        type: [String],
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.Now
+    }
+})
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema); 
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+//load profile model
+const Profile = require('../../models/Profile');
+//load  profile
+const User = require('../../models/User');
+
 
 //GET api/posts/test
 //Tests post route
 //Public route
 router.get('/test', (req, res) => res.json({msg: "Profile Works"})); 
+
+//GET api/profile
+//Get current users profile
+//Private route
+router.get(
+    '/',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+      const errors = {};
+  
+      Profile.findOne({ user: req.user.id })
+        .populate('user', ['name', 'avatar'])
+        .then(profile => {
+          if (!profile) {
+            errors.noprofile = 'There is no profile for this user';
+            return res.status(404).json(errors);
+          }
+          res.json(profile);
+        })
+        .catch(err => res.status(404).json(err));
+    }
+  );
 
 module.exports = router; 
