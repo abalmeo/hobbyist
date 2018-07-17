284cc5586f46b62162132f1396a79e224e78504a
Adam Garling <adamgarling@Adams-MacBook-Pro.local>
2018-07-16 20:44:56 -0700

    added a user profile post route and some validation

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
 
+//load validation
+const validateProfileInput = require('../../validation/profile')
+
 //load profile model
 const Profile = require('../../models/Profile');
 //load  profile
@@ -36,30 +39,68 @@ router.get(
     }
   );
 
-  //POST api/profile
-//Create user profile
+//POST api/profile
+//Create or Edit user profile
 //Private route
-router.get(
-    '/',
+router.post('/',
     passport.authenticate('jwt', { session: false }),
     (req, res) => {
-     // Get inpuits
+        const { errors, isValid } = validateProfileInput(req.body);
+
+            //Check Validation
+            if(!isValid) {
+                //return errors 
+                return res.status(400).json(errors); 
+
+            }
+        
+            console.log(req.body); 
+     // Get inputs
      const profileInputs = {} ;
-     profileFields.user = req.user.id; 
+     profileInputs.user = req.user.id; 
      if(req.body.handle) profileInputs.handle = req.body.handle;
      if(req.body.location) profileInputs.location = req.body.location;
      if(req.body.bio) profileInputs.bio = req.body.bio;
-
      //The next tnree inputs need to separated to be put into an array.
-     if(typeof req.body.skills !== 'undefined') {
-         profileInputs.skills = req.body.skills.split(',');
+        if (typeof req.body.skills !== 'undefined') {
+            profileInputs.skills = req.body.skills.split(',');
         }
-     if(typeof req.body.equipment !== 'undefined') {
-         profileInputs.equipment = req.body.equipment.split(',');
+        if (typeof req.body.equipment !== 'undefined') {
+            profileInputs.equipment = req.body.equipment.split(',');
         }
-     if(typeof req.body.interests !== 'undefined') {
-         profileInputs.interests = req.body.interests.split(','); 
+
+        if (typeof req.body.interests !== 'undefined') {
+            profileInputs.interests = req.body.interests.split(',');
         }
+
+        Profile.findOne({ user: req.user.id })
+        .then(profile => {
+
+            //make sure profile exists
+            if(profile) {
+                Profile.findOneAndUpdate(
+                    { user: req.user.id },
+                    { $set: profileInputs },
+                    { new: true }
+                )
+                .then(profile => res.json(profile));
+            } else {
+
+                //Create
+                //Check if handle exists
+                Profile.findOne({ handle: profileInputs.handle })
+                .then(profile => {
+                    if(profile) {
+                        errors.handle = 'Thant handle already exists';
+                        res.status(400).json(errors); 
+                    }
+
+                    //save profile
+                    new Profile(profileInputs).save()
+                    .then(profile => res.json(profile));
+                })
+            }
+        })
     }
   );
 
